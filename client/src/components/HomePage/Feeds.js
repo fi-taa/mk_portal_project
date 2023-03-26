3b626c9a2b65b7ff512c1dc38840033fadcdabf3
Fita-Wegene <fitaawd@gmail.com>
2023-03-26 16:54:23 +0300

    smchnge added

--- client/src/components/HomePage/Feeds.js
+++ client/src/components/HomePage/Feeds.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Feeds() {
   return (
-    <div>Feeds</div>
+    <div className='flex justify-center items-center w-1/2 h-1/2 border-slate-900'>
+      
+    </div>
   )
 }
 
